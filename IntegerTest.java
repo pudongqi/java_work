a68d6a5aa7953e5214fbaa0ad82c24c6c3573177
pudongqi <pudongqi@gmail.com>
2018-06-07 10:21:15 +0800

    new

---
+++ IntegerTest.java
@@ -0,0 +1,13 @@
+public class IntegerTest{
+
+	public static void main( String[] args ){
+		Integer intA = 2;
+		Integer intB = 2;
+
+		Integer intC = 128;
+		Integer intD = 128;
+
+		System.out.println(intA == intB);
+		System.out.println(intC == intD);
+	} 
+}
